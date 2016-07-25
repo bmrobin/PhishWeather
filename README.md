2294fb419acb866bf3a778c4f0e3c95fe558317a
Ben Robinson <brobinson@5amsolutions.com>
2016-07-25 11:58:47 -0400

    refactoring for new api call and minute code improvements. adding readme

---
+++ README.md
@@ -0,0 +1,11 @@
+## Retrieve weather info from wunderground.com for a given zip code
+
+#### Requirements
+* wunderground API key
+* gradle 2.x
+* java >= 1.7
+
+#### Installation (for IntelliJ IDEA)
+    $ ./gradlew clean build idea
+
+Configure zip code and wunderground API key via command line arguments in IntelliJ run configurations. Then launch main method in ```WeatherAlert.java``` to execute the program.
--- src/main/java/bmrobin/WeatherAlertRetriever.java
+++ src/main/java/bmrobin/WeatherAlertRetriever.java
@@ -1,6 +1,5 @@
 package bmrobin;
 
-
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -9,37 +8,37 @@ import java.util.Map;
 import java.util.Scanner;
 import java.net.URL;
 import java.util.HashMap;
-import java.lang.StringBuilder;
 
 public class WeatherAlertRetriever {
-	
-	// Vienna ZIP code URL example
+
+	// Vienna ZIP code URL example using bmrobin's API key
 	// http://api.wunderground.com/api/a165779ec885eb6c/conditions/q/22180.json
-	
-	private final String apiKey = "a165779ec885eb6c";
+
+	private final String apiKey;
 	private final String zipCode;
-	private String weatherURL = "http://api.wunderground.com/api/" + apiKey + "/conditions/q/";
-	private HashMap<String, String> weatherInfo;
-	
-	public WeatherAlertRetriever(String zip) {
+	private String weatherURL;
+	private Map<String, String> weatherInfo;
+
+	public WeatherAlertRetriever(String zip, String apiKey) {
 		this.zipCode = zip;
-		this.weatherURL = weatherURL + zipCode + ".json";
+        this.apiKey = apiKey;
+		this.weatherURL = "http://api.wunderground.com/api/" + this.apiKey + "/conditions/q/" + this.zipCode + ".json";
 		weatherInfo = new HashMap<String, String>();
-//		System.out.println("Using zip code: " + this.zipCode);
+		System.out.println("Using zip code: " + this.zipCode);
 	}
-	
+
 	public JSONObject getJSONWeather() {
 		JSONObject jObj = null;
 		JSONObject tempObj = null;
-		JSONArray jArray = null;
-		JSONArray tempArr = null;
+		JSONArray jArray;
+		JSONArray tempArr;
 		
 		try {
 			// request JSON of current conditions from api
-//			System.out.println("Getting weather from api: " + this.weatherURL);
+			System.out.println("Getting weather from api: " + this.weatherURL);
 			URL addr = new URL(this.weatherURL);
 			Scanner scan = new Scanner(addr.openStream());
-			String str = new String();
+			String str = "";
 			while (scan.hasNext()) {
 				str += scan.nextLine();
 			}
@@ -53,30 +52,31 @@ public class WeatherAlertRetriever {
 		
 		try {
 			// create an array of the 2nd grouping in the JSON object
-			tempArr = tempObj.toJSONArray(tempObj.names());
-			jArray = new JSONArray("[" + tempArr.get(1).toString() + "]");
-			jObj = jArray.getJSONObject(0);
-		}
-		catch (Exception e) {
+			if (tempObj != null) {
+				tempArr = tempObj.toJSONArray(tempObj.names());
+				jArray = new JSONArray("[" + tempArr.get(1).toString() + "]");
+				jObj = jArray.getJSONObject(0);
+			}
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		
 		return jObj;
 	}
-	
-	public void storeJSONWeather(JSONObject obj) {
+
+    public void format(JSONObject obj) {
 		try {
 			// location is nested inside another JSON array
 			JSONObject locationObj = obj.getJSONObject("display_location");
 			String location = locationObj.getString("full");
 			
-			String farenheit = obj.getString("temp_f");
+			Double farenheit = (Double) obj.get("temp_f");
 			String timestamp = obj.getString("observation_time");
 			String weather = obj.getString("weather");
 			String feelsLikeFarenheit = obj.getString("feelslike_f");
 			
 			this.weatherInfo.put("Location", location);
-			this.weatherInfo.put("Current Temp (F)", farenheit);
+			this.weatherInfo.put("Current Temp (F)", farenheit.toString());
 			this.weatherInfo.put("Time", timestamp);
 			this.weatherInfo.put("Conditions", weather);
 			this.weatherInfo.put("Feels Like (F)", feelsLikeFarenheit);
@@ -90,7 +90,7 @@ public class WeatherAlertRetriever {
 		StringBuilder str = new StringBuilder();
 		str.append("----------\n");
 		for (Map.Entry<String,String> entry : this.weatherInfo.entrySet()) {
-			str.append(entry.getKey() + ": " + entry.getValue() + "\n");
+			str.append(entry.getKey()).append(": ").append(entry.getValue()).append("\n");
 		}
 		str.append("----------");
 		return str.toString();
