3544bed8eaae7c67633210013f32504b815d9a41
Ben Robinson <brobinson@5amsolutions.com>
2017-03-03 10:07:02 -0500

    using jquery to load weather data onto page or display error

--- src/main/webapp/scripts/app/app.ts
+++ src/main/webapp/scripts/app/app.ts
@@ -1,16 +1,10 @@
 import { LocationService } from "./services/locationService";
 
 class App {
-    private greeting: string = "learning typescript :)";
     private locationService = new LocationService();
 
     constructor() {
-        this.setGreeting(this.greeting);
         this.locationService.getLocationData("26209");
-    }
-
-    public setGreeting(greeting: string) {
-        $("#test").text(greeting);
     }
 }
 
