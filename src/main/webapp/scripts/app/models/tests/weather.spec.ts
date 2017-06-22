b1e8ec3603cfe82953dbd5d689f9381eb614428f
Ben Robinson <brobinson@5amsolutions.com>
2017-06-22 15:24:34 -0400

    adding base model test coverage

--- src/main/webapp/scripts/app/models/tests/weather.spec.ts
+++ src/main/webapp/scripts/app/models/tests/weather.spec.ts
@@ -6,6 +6,8 @@ describe("Models: Weather", () => {
     let jsonData = {averageTempFahrenheit: "75", date: "2017-06-21"};
     let weather: Weather = new Weather(jsonData);
     expect(weather).not.toBe(null);
+    expect(weather.averageTempFahrenheit).toBe("75");
+    expect(weather.date).toBe("2017-06-21");
   });
 
 });
