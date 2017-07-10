1d7e9dfb1af615f167dc8ccf698d323ab092d410
Ben Robinson <benjamin.robinson@cengage.com>
2017-07-10 14:18:39 -0400

    commenting webpack file for better understanding

--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,7 @@ module.exports = {
 	module: {
 		rules: [
 			{
+				// Before running any of our loaders, lint our code with ESLint
 				enforce: "pre",
 				test: /\.jsx?$/,
 				exclude: /node_modules/,
@@ -21,25 +22,30 @@ module.exports = {
 				}
 			},
 			{
+				// transpile our ES6 code with Babel
 				test: /\.jsx?$/,
 				exclude: /node_modules/,
 				loader: "babel-loader"
 			},
 			{
+				// enable source maps for our transpiled code
 				test: /\.jsx?$/,
 				loader: 'source-map-loader'
 			},
 			{
+				// extract the CSS we imported as modules and inject it into the transpiled JS code
 				test: /\.css$/,
 				loader: 'style-loader!css-loader'
 			}
 		]
 	},
 	plugins: [
+		// injects our bundled JS code into our HTML file inside a <script> tag
 		new HtmlWebpackPlugin({
 			inject: true,
 			template: './src/main/webapp/index.html'
 		}),
+		// enable hot replacement on our dev server
 		new webpack.HotModuleReplacementPlugin()
 	],
 	stats: {
@@ -50,6 +56,7 @@ module.exports = {
 		extensions: ['.jsx', '.js']
 	},
 	devtool: 'source-map',
+	// configure a dev server that can watch and reload when files change
 	devServer: {
 		port: 9000,
 		compress: true,
